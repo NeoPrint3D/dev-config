fb681e827593ac4a8f078c6cf8b1e8425266a621
NeoPrint3D <drew@ronsman.com>
2024-03-04 08:53:21 -0600

    new browser config

--- scripts/linux/setup.sh
+++ scripts/linux/setup.sh
@@ -11,6 +11,8 @@ git config --global user.email "drew@ronsman.com"
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
