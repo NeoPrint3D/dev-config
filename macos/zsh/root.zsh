4df02ee5bff663e4362cc6eb2b67c9ab068b3ec7
drew <drew@ronsman.com>
2024-03-26 20:30:18 -0500

    new stuff

--- macos/zsh/root.zsh
+++ macos/zsh/root.zsh
@@ -1,5 +1,7 @@
 export ZSH="$HOME/.oh-my-zsh"
 export ZDOTDIR="$HOME/dev-config/macos/zsh"
 source ~/dev-config/macos/.profile
+source ~/dev-config/macos/zsh/.zshrc
 exec zsh
+
 
--- scripts/macos/push-configs.sh
+++ scripts/macos/push-configs.sh
@@ -1,8 +1,3 @@
-echo "Installing Homebrew"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-echo "Installing Brew Packages"
-brew bundle --file=~/dev-config/macos/Brewfile
 echo "Installing zsh config"
 cp ~/dev-config/macos/zsh/root.zsh ~/.zshrc
 echo "Installing nvim config"
