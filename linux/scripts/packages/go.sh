5879177a47ccb468c017b1bc234967ffbb8df397
NeoPrint3D <drew@ronsman.com>
2024-05-03 21:29:23 -0500

    some fixes

--- linux/scripts/packages/go.sh
+++ linux/scripts/packages/go.sh
@@ -1,7 +1,9 @@
-GO_ARCHIVE=$(curl -s 'https://go.dev/VERSION?m=text' | head -n 1).linux-arm64.tar.gz
+GO_ARCHIVE=$(curl -s 'https://go.dev/VERSION?m=text' | head -n 1).linux-amd64.tar.gz
 GO_LINK="https://go.dev/dl/$GO_ARCHIVE"
 wget $GO_LINK
-sudo rm -rf "$HOME/go"
+sudo rm -rf "$HOME/.go"
 mkdir -p "$HOME/.go"
 sudo tar -C "$HOME/.go" -xzf $GO_ARCHIVE
+# move .go/go/* to .go/*
+mv "$HOME/.go/go"/* "$HOME/.go"
 rm $GO_ARCHIVE
--- linux/zsh/root.zsh
+++ linux/zsh/root.zsh
@@ -1,3 +1,4 @@
+export ZSH="$HOME/.oh-my-zsh"
 export ZDOTDIR="$HOME/dev-config/linux/zsh"
 source ~/dev-config/linux/.profile
 exec zsh
